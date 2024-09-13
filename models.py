b8f4935062eeb0e49593d559a72841d6de45049b
frajescaresp <76232700+frajescaresp@users.noreply.github.com>
2024-09-13 14:10:24 -0600

    Create models.py

---
+++ models.py
@@ -0,0 +1,19 @@
+from app import db
+
+class User(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(100), unique=True, nullable=False)
+    password = db.Column(db.String(100), nullable=False)
+    profile_pic = db.Column(db.String(100), default='default.jpg')
+    cover_pic = db.Column(db.String(100), default='default_cover.jpg')
+    
+class Post(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    content = db.Column(db.String(200), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey('user.id'))
+
+class FriendRequest(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    from_user = db.Column(db.Integer, db.ForeignKey('user.id'))
+    to_user = db.Column(db.Integer, db.ForeignKey('user.id'))
